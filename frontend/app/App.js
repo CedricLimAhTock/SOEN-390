11433c5fbfc795d5b3742d3af7ea020f4b34959b
ACCISA <aniss.chalah@gmail.com>
2025-01-21 20:45:52 -0500

    github repo setup

---
+++ frontend/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import "../global.css"
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-500">
+      <Text className="text-white text-xl">Hello Tassilwind in React Native!</Text>
+    </View>
+  );
+}
+
