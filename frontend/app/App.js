0fb7a7889a22879eb6a8a0c8c443a6b12d813d82
ACCISA <aniss.chalah@gmail.com>
2025-01-23 14:49:17 -0500

    frontend structure

--- frontend/app/App.js
+++ frontend/app/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 import "../global.css"
+import HomeScreen from './screens/home/HomeScreen';
+import CalendarScreen from './screens/calendar/CalendarScreen';
+import NavigationScreen from './screens/navigation/NavigationScreen';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-500">
-      <Text className="text-white text-xl">Hellss Tassilwind in React Native!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Calendar" component={CalendarScreen} />
+        <Stack.Screen name="Navigation" component={NavigationScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
