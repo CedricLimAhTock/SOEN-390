6a57c26e014c66a5ffd64e96c0b5452306ea45f5
SoumyBhow <soumybhow1.0@gmail.com>
2025-02-04 22:29:20 -0500

    Hide headers

--- frontend/app/App.js
+++ frontend/app/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import "../global.css"
-import HomeScreen from './screens/home/HomeScreen';
-import CalendarScreen from './screens/calendar/CalendarScreen';
-import NavigationScreen from './screens/navigation/NavigationScreen';
+import "../global.css";
+import HomeScreen from "./screens/home/HomeScreen";
+import CalendarScreen from "./screens/calendar/CalendarScreen";
+import NavigationScreen from "./screens/navigation/NavigationScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Calendar" component={CalendarScreen} />
         <Stack.Screen name="Navigation" component={NavigationScreen} />
@@ -21,4 +24,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
