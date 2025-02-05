09ccef577dc00ba7d73fe5c8891a2caedb64fe76
SoumyBhow <soumybhow1.0@gmail.com>
2025-02-04 22:34:38 -0500

    Round corners more + add shadow to home header

--- frontend/app/components/HomeHeader/HomeHeader.jsx
+++ frontend/app/components/HomeHeader/HomeHeader.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import { View, Text, Button, Pressable } from 'react-native';
+import React from "react";
+import { View, Text, Button, Pressable } from "react-native";
 
 export default function HomeHeader() {
   return (
-    <View className="rounded-xl" style={{height:194,   alignItems: 'center', flexDirection: 'row', backgroundColor:'#862532'}}>
-<View className="flex pl-6 pt-5">
-<Text className="text-white font-bold text-3xl">
-    Welcome Back
-</Text>
-<Text className="text-white font-bold text-2xl">
-    Joe Smith
-</Text>
-</View>
+    <View
+      style={{
+        height: 194,
+        alignItems: "center",
+        flexDirection: "row",
+        backgroundColor: "#862532",
+        borderBottomLeftRadius: 60,
+        borderBottomRightRadius: 60,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.55,
+        shadowRadius: 3.84,
+        elevation: 5,
+      }}
+    >
+      <View className="flex pl-6 pt-5">
+        <Text className="text-white font-bold text-3xl">Welcome Back</Text>
+        <Text className="text-white font-bold text-2xl">Joe Smith</Text>
+      </View>
     </View>
   );
 }
