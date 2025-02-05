1a99c81b057bbf2d3b1f42a60ed6103f8a29bc36
SoumyBhow <soumybhow1.0@gmail.com>
2025-02-04 22:07:50 -0500

    Add cards to home page

--- frontend/app/screens/home/HomeScreen.jsx
+++ frontend/app/screens/home/HomeScreen.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import BottomNavBar from '../../components/BottomNavBar/BottomNavBar';
-import HomeHeader from '../../components/HomeHeader/HomeHeader';
-
+import React from "react";
+import { View, Text, Button } from "react-native";
+import BottomNavBar from "../../components/BottomNavBar/BottomNavBar";
+import HomeHeader from "../../components/HomeHeader/HomeHeader";
+import HomeCard from "../../components/HomeCard/HomeCard";
+import MapPic from "../../../assets/MapScreenshot.png";
+import CalendarPic from "../../../assets/CalendarScreenshot.png";
 export default function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center',  }}>
-      <HomeHeader/>
-
-      <BottomNavBar/>
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      <HomeHeader />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          gap: 20,
+          paddingTop: 160,
+        }}
+      >
+        <HomeCard image={MapPic} text="Find your next class" />
+        <HomeCard image={CalendarPic} text="Access your calendar" />
+      </View>
+      <BottomNavBar />
     </View>
   );
 }
