51dda6538df276b08641eac48947afc36554e6ad
Andrew Harissi Dagher <andrew.h.dagher@gmail.com>
2025-02-06 14:49:20 -0500

    initial CI/CD pipeline yaml

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -1,7 +1,15 @@
-jest.mock('../app', () => {
-    const express = require('express');
-    const app = express();
-    app.listen = jest.fn();
-    return app;
+const request = require('supertest');
+const express = require('express');
+const routes = require('../startup/routes');
+const { json } = require('body-parser');
+
+const app = express();
+routes(app);
+
+describe('GET /test', () => {
+  it('should return 200 OK', async () => {
+    const response = await request(app).get('/test');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe,json('Test Router OK');
   });
-  
+});
