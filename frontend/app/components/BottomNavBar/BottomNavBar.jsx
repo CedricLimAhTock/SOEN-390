55abeba06bda33fcd47596f9d17498a0a522bf83
SoumyBhow <soumybhow1.0@gmail.com>
2025-02-07 10:40:08 -0500

    Switch nav and calendar buttons on bottom nav to match mockup

--- frontend/app/components/BottomNavBar/BottomNavBar.jsx
+++ frontend/app/components/BottomNavBar/BottomNavBar.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { View, Pressable, StyleSheet } from 'react-native';
-import { useNavigation, useRoute } from '@react-navigation/native';
+import React from "react";
+import { View, Pressable, StyleSheet } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import HomeActive from "./HomeIcons/HomeActive";
 import HomeInactive from "./HomeIcons/HomeInactive";
 import CalendarActive from "./CalendarIcons/CalendarActive";
@@ -15,39 +15,38 @@ export default function BottomNavBar() {
   const route = useRoute();
 
   return (
-    
     <View style={styles.container}>
-      
       <Pressable onPress={() => navigation.navigate("Home")}>
         {route.name === "Home" ? <HomeActive /> : <HomeInactive />}
       </Pressable>
 
-      
+      <Pressable onPress={() => navigation.navigate("Navigation")}>
+        {route.name === "Navigation" ? (
+          <NavigationActive />
+        ) : (
+          <NavigationInactive />
+        )}
+      </Pressable>
+
       <Pressable onPress={() => navigation.navigate("Calendar")}>
         {route.name === "Calendar" ? <CalendarActive /> : <CalendarInactive />}
       </Pressable>
 
-      
-      <Pressable onPress={() => navigation.navigate("Navigation")}>
-        {route.name === "Navigation" ? <NavigationActive /> : <NavigationInactive />}
-      </Pressable>
-
-     
       <Pressable onPress={() => navigation.navigate("Settings")}>
         {route.name === "Settings" ? <SettingsActive /> : <SettingsInactive />}
       </Pressable>
-    </View> 
-    );
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        height: 60,
-        backgroundColor: '#FFFFFF',
-        borderTopWidth: 1,
-        borderTopColor: '#d6d6d6',
-    },
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    height: 60,
+    backgroundColor: "#FFFFFF",
+    borderTopWidth: 1,
+    borderTopColor: "#d6d6d6",
+  },
 });
